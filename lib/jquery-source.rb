ea16aab9d64f145111baf5d4439eff2764a868fc
Daniel X. Moore <yahivin@gmail.com>
2012-06-01 14:37:00 -0700

    Wiring up for sprockets and rails

--- lib/jquery-source.rb
+++ lib/jquery-source.rb
@@ -1,7 +1,13 @@
 require "jquery-source/version"
 
+# Check for Rails or Sprockets
+if defined? ::Rails::Engine
+  require "jquery-source/rails"
+elsif defined? ::Sprockets
+  require "jquery-source/sprockets"
+end
+
 module Jquery
   module Source
-    # Your code goes here...
   end
 end
